4c4fff9f00b69cf60530f645aa0f5bd707b47e69
stefancvetincanin <stefancvetincanin@gmail.com>
2019-03-14 17:31:54 +0100

    fix notifikacija

--- notifikacija.js
+++ notifikacija.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
   ws.addEventListener('message', e => {
     console.log(e.data) // TODO: prikazati notifikaciju u headeru
     $("#notifikacija").slideDown()
+
+    //reject unauthorized false
   })
   $("#notifikacija-close").on("click", function() {
     $("#notifikacija").slideUp()
   })
 })
+
+//dodati jedan s za secure u ws
