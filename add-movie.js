d7dae0ea8752db083c07d033518a7e1c1e8177ee
stefancvetincanin <stefancvetincanin@gmail.com>
2019-03-22 17:10:09 +0100

    noz u ledja

--- add-movie.js
+++ add-movie.js
@@ -1,7 +1,27 @@
-// document.getElementById("add-movie").addEventListener("submit", function(e) {
-//   e.preventDefault()
-// })
+const s = selektor => document.getElementById(selektor)
 
 $('[name="slika"]').on('change', function () {
   $('img.preview').prop('src', this.value);
 });
+
+s("add-movie").addEventListener("submit", function (e) {
+  e.preventDefault()
+  const movieInfo = {
+    naziv: s("naziv").value,
+    godina: s("godina").value,
+    slika: s("slika").value
+  }
+  fetch('https://baza-filmova.herokuapp.com/dodaj-film/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(movieInfo)
+  })
+    .then(response => response.text())
+    .then(response => {
+      console.log(response)
+      s("post-info").innerHTML = response
+    })
+})
